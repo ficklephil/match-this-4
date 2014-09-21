ac535df40fe096859c955090473dce0a4378aa9a
Philip <ficklephil@hotmail.com>
2014-09-21 07:27:43 +0100

    Cleaning

--- client/app/results/results.controller.js
+++ client/app/results/results.controller.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('matchThis4App')
-  .controller('ResultsCtrl', function ($scope,$rootScope, $stateParams) {
-    $scope.message = 'Hello';
+    .controller('ResultsCtrl', function ($scope,$rootScope, $http) {
+        $scope.message = 'Hello';
 
-    console.log('cameraPalette');
-    console.log($rootScope.cameraPalette);
-    console.log('cameraColor');
-    console.log($rootScope.cameraColor);
-  });
+            console.log('cameraPalette');
+            console.log($rootScope.cameraPalette);
+            console.log('cameraColor');
+            console.log($rootScope.cameraColor);
+
+        $http.get('/api/product/red+dresses').success(function(data) {
+            console.log(data);
+        });
+    });
